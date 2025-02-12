ddadb3539850f41c61ee72de23a3acb9a522fba2
braguru <sabastainofori@gmail.com>
2025-02-12 12:46:47 +0000

    modified code

--- main.tf
+++ main.tf
@@ -11,10 +11,33 @@ resource "aws_s3_bucket" "static_site" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "static_site" {
+  bucket                  = var.bucket_name
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
 
-resource "aws_s3_bucket_website_configuration" "site_configuration"{
-    bucket = var.bucket_name
-    index_document {
+resource "aws_s3_object" "index" {
+  bucket       = var.bucket_name
+  key          = "index.html"
+  source       = "${path.module}/index.html"
+  content_type = "text/html"
+  depends_on   = [aws_s3_bucket.static_site]
+}
+
+resource "aws_s3_object" "error" {
+  bucket       = var.bucket_name
+  key          = "error.html"
+  source       = "${path.module}/error.html"
+  content_type = "text/html"
+  depends_on   = [aws_s3_bucket.static_site]
+}
+
+resource "aws_s3_bucket_website_configuration" "site_configuration" {
+  bucket = var.bucket_name
+  index_document {
     suffix = "index.html"
   }
 
@@ -24,17 +47,21 @@ resource "aws_s3_bucket_website_configuration" "site_configuration"{
 }
 
 resource "aws_s3_bucket_policy" "public_policy" {
-    bucket = aws_s3_bucket.static_site.id 
-    policy = jsonencode({
-        Version = "2012-10-17", 
-        Statement = [ 
-            {
-            Sid = "PublicReadGetObject", 
-            Effect = "Allow", Principal = "*",
-            Action = ["s3:GetObject"], 
-            Resource = ["${aws_s3_bucket.static_site.arn}/*"] 
-            } 
-        ] 
-    })
+  bucket = aws_s3_bucket.static_site.id
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": [ "s3:GetObject"],
+        "Resource": [
+          "${aws_s3_bucket.static_site.arn}/*"
+        ]
+      }
+    ]
+  }
+  EOF
 }
 
